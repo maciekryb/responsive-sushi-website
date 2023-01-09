9a584a0db1367adb5a49dc7d17ef75f121750a2f
Maciej Rybak <maciek.rybak95@gmail.com>
2023-01-10 00:12:34 +0100

    add button css and header box shadow

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,13 +25,21 @@ if (navClose) {
 const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-  const navMenu = document.getElementById("nav-menu");
+  // const navMenu = document.getElementById("nav-menu");
   //When we click on each nav__link, we remove the show-menu class
   navMenu.classList.remove("show-menu");
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 10
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
 
