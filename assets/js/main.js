bb500673cea32934c41a01c330e4b757d08315d6
Maciej Rybak <maciek.rybak95@gmail.com>
2023-01-15 18:56:19 +0100

    add  scroll sections active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,15 +35,39 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 const scrollHeader = () => {
   const header = document.getElementById("header");
-  this.scrollY >= 50
-    ? header.classList.add("bg-header")
-    : header.classList.remove("bg-header");
+  this.scrollY >= 50 ? header.classList.add("bg-header") : header.classList.remove("bg-header");
 };
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350 ? scrollUp.classList.add("show-scroll") : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+console.log(sections);
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight, //wysokosc sekcji
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionClass = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
 
